92edf1181a9f61328f1d85fdacc90caf9c31965a
WAI <93478503+WAIXv@users.noreply.github.com>
2021-11-30 21:31:28 +0800

    11.30

---
+++ GravityWell/Assets/Script/AbsorbObject.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbsorbObject : MonoBehaviour
+{
+    public float magnetForce = 100;
+
+    List<Rigidbody2D> caughtRigidbodies = new List<Rigidbody2D>();
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < caughtRigidbodies.Count; i++)
+        {
+            caughtRigidbodies[i].velocity = (transform.position - caughtRigidbodies[i].transform.position) * magnetForce * Time.deltaTime ;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>())
+        {
+            Rigidbody2D r = other.GetComponent<Rigidbody2D>();
+
+            if(!caughtRigidbodies.Contains(r))
+            {
+                //Add Rigidbody
+                caughtRigidbodies.Add(r);
+                Debug.Log(other.tag);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>())
+        {
+            Rigidbody2D r = other.GetComponent<Rigidbody2D>();
+
+            if (caughtRigidbodies.Contains(r))
+            {
+                //Remove Rigidbody
+                caughtRigidbodies.Remove(r);
+                Debug.Log(other.tag);
+            }
+        }
+    }
+}
