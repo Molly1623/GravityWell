665dd24adb4e5e56d6d9635e9aabd53608cdf175
Molly1623 <912814725@qq.com>
2021-12-03 20:45:07 +0800

    update: Code formate

--- GravityWell/Assets/Script/Razor/RedLine.cs
+++ GravityWell/Assets/Script/Razor/RedLine.cs
@@ -2,29 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class RedLine : MonoBehaviour
+namespace GravityWell.Razor
 {
-    // Start is called before the first frame update
-    void Start()
+    public class RedLine : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //触碰红色射线
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
+        // Start is called before the first frame update
+        void Start()
         {
-            GameObject.Find("Canvas").transform.Find("Panel").gameObject.SetActive(true);
-            collision.GetComponent<Rigidbody2D>().gravityScale = 0;
-            collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        //触碰红色射线
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.tag == "Player")
+            {
+                GameObject.Find("Canvas").transform.Find("Panel").gameObject.SetActive(true);
+                collision.GetComponent<Rigidbody2D>().gravityScale = 0;
+                collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
         }
     }
 }
+
--- GravityWell/Assets/Script/Razor/Restart.cs
+++ GravityWell/Assets/Script/Razor/Restart.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class Restart : MonoBehaviour
+namespace GravityWell.Razor
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Restart : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
 
-    }
+        }
 
-    public void Replay()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        public void Replay()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
